b258e1412d92e1ba0eae1ee44a83b42e88969b13
tomzacchia <tomzacchia@live.com>
2022-02-11 12:05:43 -0800

    Add event listener to window resize
    - change background images dynamically based on viewport width

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,3 +1,11 @@
 import "./about-us.scss";
 
-console.log("hi");
+window.addEventListener("resize", function () {
+  const windowWitdh = window.innerWidth;
+
+  if (windowWitdh >= 768) {
+    const card = document.querySelector(".portfolio-card-1");
+    card.style.backgroundImage =
+      "url(../assets/portfolio/mobile/image-del-sol.jpg)";
+  }
+});
