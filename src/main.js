bc91c48c6abb306622a17f8cd1106d0b94dc5874
tomzacchia <tomzacchia@live.com>
2022-02-10 20:42:34 -0800

    Add all linkable element

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ const hamburger = document.querySelector(".hamburger");
 const darkOverlayNode = document.querySelector(".dark-overlay");
 const menuItemsContainer = document.querySelector(".menu");
 const MENU_ANIMATION_CLASSNAME = "animate-menu";
+const featuredCardContainer = document.querySelector(
+  ".featured-cards-container"
+);
 
 hamburger.addEventListener("click", () => {
   if (is_bars) {
@@ -38,3 +41,68 @@ function removeMenuAnimateClassIfExists() {
     return;
   }
 }
+
+// background-image: url("assets/portfolio/mobile/image-del-sol.jpg")
+const featuredCardConfig = [
+  {
+    title: "Project Del Sol",
+    bgImageUrl: "/assets/portfolio/mobile/image-del-sol.jpg",
+  },
+  {
+    title: "228B Tower",
+    bgImageUrl: "/assets/portfolio/mobile/image-228b.jpg",
+  },
+  {
+    title: "Le Prototype",
+    bgImageUrl: "/assets/portfolio/mobile/image-prototype.jpg",
+  },
+];
+
+function getFeaturedCardHTML() {
+  return featuredCardConfig
+    .map((config, index) => {
+      return `
+            <div class="featured-card featured-${index}" style="background-image: url(${config.bgImageUrl})">
+          <h1 class="h1-large"> ${index} </h1>
+          <div class="linear-overlay"></div>
+          <div class="featured-card-text">
+            <h3 class="h3"> ${config.title} </h3>
+            <p class="pointer">
+              <a href="index.html" style="color: inherit">View All Projects </a>
+            </p>
+          </div>
+        </div>
+    `;
+    })
+    .join(" ");
+}
+
+featuredCardContainer.insertAdjacentHTML("beforeend", getFeaturedCardHTML());
+
+// <div class="featured-card featured-1">
+//   <h1 class="h1-large"> 1 </h1>
+//   <div class="linear-overlay"></div>
+//   <div class="featured-card-text">
+//     <h3 class="h3"> Project Del Sol </h3>
+//     <p class="pointer">
+//       <a href="index.html" style="color: inherit">View All Projects </a>
+//     </p>
+//   </div>
+// </div>
+//   <div class="featured-card featured-2">
+//     <h1 class="h1-large"> 2 </h1>
+//     <div class="linear-overlay"></div>
+//     <div class="featured-card-text">
+//       <h3 class="h3"> 228B Tower </h3>
+//       <p class="pointer"> View All Projects </p>
+//     </div>
+//   </div>
+//   <div class="featured-card featured-3">
+//     <h1 class="h1-large"> 3 </h1>
+//     <div class="linear-overlay"></div>
+//     <div class="featured-card-text">
+//       <h3 class="h3"> Le Prototype </h3>
+//       <p class="pointer"> View All Projects </p>
+//     </div>
+//   </div>
+// </div>
