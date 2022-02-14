788a05638747954eb51c0fcb1e806fa8c8a3858d
tomzacchia <tomzacchia@live.com>
2022-02-14 11:05:23 -0500

    Add getImageUrlForMediaWidth helper function

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -91,19 +91,13 @@ const portfolioContainer = document.querySelector(".portfolio");
 
 const cardsHTML = cardsConfig
   .map((card, index) => {
-    const windowWitdh = window.innerWidth;
-    let bgImageUrl = card.bgImageUrlMobile;
-
-    if (windowWitdh >= 1440) {
-      bgImageUrl = card.bgImageUrlDesktop;
-    } else if (windowWitdh >= 768) {
-      bgImageUrl = card.bgImageUrlTablet;
-    }
+    const windowWidth = window.innerWidth;
+    const bgImageUrl = getImageUrlForMediaWidth(windowWidth, card);
 
     return `
         <div class="portfolio-card portfolio-card-${
           index + 1
-        }" style="background-image: url(${bgImageUrl})">
+        }" style="background-image: ${bgImageUrl}">
         <div class="linear-overlay"></div>
         <div class="portfolio-card-text">
           <h3 class="h3"> ${card.name} </h3>
@@ -121,17 +115,22 @@ window.addEventListener("resize", function () {
   const windowWitdh = window.innerWidth;
   const cards = document.querySelectorAll(".portfolio-card");
 
-  if (windowWitdh >= 768 && windowWitdh < 1440) {
-    cards.forEach((card, index) => {
-      card.style.backgroundImage = `url(${cardsConfig[index].bgImageUrlTablet})`;
-    });
-  } else if (windowWitdh >= 1440) {
-    cards.forEach((card, index) => {
-      card.style.backgroundImage = `url(${cardsConfig[index].bgImageUrlDesktop})`;
-    });
-  } else {
-    cards.forEach((card, index) => {
-      card.style.backgroundImage = `url(${cardsConfig[index].bgImageUrlMobile})`;
-    });
-  }
+  cards.forEach((card, index) => {
+    card.style.backgroundImage = getImageUrlForMediaWidth(
+      windowWitdh,
+      cardsConfig[index]
+    );
+  });
 });
+
+function getImageUrlForMediaWidth(windowWidth, cardConfig) {
+  let imageUrl = `url(${cardConfig.bgImageUrlMobile})`;
+
+  if (windowWidth >= 1440) {
+    imageUrl = `url(${cardConfig.bgImageUrlDesktop})`;
+  } else if (windowWidth >= 768) {
+    imageUrl = `url(${cardConfig.bgImageUrlTablet})`;
+  }
+
+  return imageUrl;
+}
