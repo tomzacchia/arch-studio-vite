ccc07defe049da30eda0a710ff766d59c89f51f2
tomzacchia <tomzacchia@live.com>
2022-02-14 12:10:57 -0500

    Update vite.config to clear build dir

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   root: "src",
   build: {
     outDir: "../dist",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
