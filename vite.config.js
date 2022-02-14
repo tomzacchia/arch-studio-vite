a3120285cc09d2e428791ee7bb8aa611b98cb4ef
tomzacchia <tomzacchia@live.com>
2022-02-14 13:22:04 -0500

    Refactor <head> into `pre-transform` html plugin

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,55 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        nested: resolve(__dirname, "src/pages/about-us.html"),
+        about: resolve(__dirname, "src/about/index.html"),
       },
     },
   },
+  plugins: [htmlPlugin()],
 });
+
+function htmlPlugin() {
+  return {
+    name: "pre-transform",
+    transformIndexHtml: {
+      enforce: "pre",
+      transform(html, { filename }) {
+        return `
+          <!DOCTYPE html>
+          <html lang="en">
+            ${headMarkup}
+
+            <body>${html}</body>
+          </html>
+        `;
+      },
+    },
+  };
+}
+
+const headMarkup = `
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="./assets/favicon-32x32.png"
+    />
+
+    <title>Arch Studio</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Spartan:wght@100;500;800&display=swap"
+      rel="stylesheet"
+    />
+    
+    <script
+      src="https://kit.fontawesome.com/349cf0313a.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+`;
