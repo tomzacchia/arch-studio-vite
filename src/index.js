67d9ab65f35de07d7ce034034c3427463ed48356
tomzacchia <tomzacchia@live.com>
2022-02-16 09:40:40 -0500

    Import image assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import Glide from "@glidejs/glide";
 import "/index.scss";
 
+import mobileParamour from "./assets/home/mobile/image-hero-paramour.jpg";
+import tabletParamour from "./assets/home/tablet/image-hero-paramour.jpg";
+import desktopParamour from "./assets/home/desktop/image-hero-paramour.jpg";
+
+import mobileSeraph from "./assets/home/mobile/image-hero-seraph.jpg";
+import tabletSeraph from "./assets/home/tablet/image-hero-seraph.jpg";
+import desktopSeraph from "./assets/home/desktop/image-hero-seraph.jpg";
+
+import mobileFederal from "./assets/home/mobile/image-hero-federal.jpg";
+import tabletFederal from "./assets/home/tablet/image-hero-federal.jpg";
+import desktopFederal from "./assets/home/desktop/image-hero-federal.jpg";
+
+import mobileTrinity from "./assets/home/mobile/image-hero-trinity.jpg";
+import tabletTrinity from "./assets/home/tablet/image-hero-trinity.jpg";
+import desktopTrinity from "./assets/home/desktop/image-hero-trinity.jpg";
+
 // Initialize after DOM has mounted
 setTimeout(() => {
   new Glide(".glide").mount();
@@ -14,28 +30,29 @@ const carouselUlContainer = document.querySelector(".glide__slides");
 featuredCardContainer.insertAdjacentHTML("beforeend", getFeaturedCardsHTML());
 carouselUlContainer.insertAdjacentHTML("afterbegin", getPrimaryHeroesHTML());
 
+// TODO: import images
 function getPrimaryHeroesHTML() {
   const configs = [
     {
-      imageName: "paramour",
+      images: [mobileParamour, tabletParamour, desktopParamour],
       title: "Project Paramour",
       description:
         "Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.",
     },
     {
-      imageName: "seraph",
+      images: [mobileSeraph, tabletSeraph, desktopSeraph],
       title: "Seraph Stations",
       description:
         "The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.",
     },
     {
-      imageName: "federal",
+      images: [mobileFederal, tabletFederal, desktopFederal],
       title: "Federal Tower II",
       description:
         "A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.",
     },
     {
-      imageName: "trinity",
+      images: [mobileTrinity, tabletTrinity, desktopTrinity],
       title: "Trinity Bank Tower",
       description:
         "Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.",
@@ -53,15 +70,15 @@ function getPrimaryHeroesHTML() {
           >
             <source
               media="(min-width:1440px)"
-              srcset="./assets/home/desktop/image-hero-${config.imageName}.jpg"
+              srcset="${config.images[2]}"
             />
             <source
               media="(min-width:768px)"
-              srcset="./assets/home/tablet/image-hero-${config.imageName}.jpg"
+              srcset="${config.images[1]}"
             />
             <img
-              src="./assets/home/mobile/image-hero-${config.imageName}.jpg"
-              alt="${config.imageName} architecture design"
+              src="${config.images[0]}"
+              alt="${config.title} architecture design"
             />
             <div class="hero-dark-overlay" style="z-index: 5"></div>
           </picture>
